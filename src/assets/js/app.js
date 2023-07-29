a74c2ff0feb5f399da06805cd677caa8c9795b91
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-29 16:38:55 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,62 @@
-const menu = document.querySelector("#menu");
-const button = document.querySelector("#menu-btn");
+const menu = document.getElementById("menu");
+const button = document.getElementById("menu-btn");
 
 button.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
+  toggleMenu();
 });
+
+function toggleMenu() {
+  menu.classList.toggle("hidden");
+  animateMenu();
+}
+
+function animateMenu() {
+  gsap.from(menu, {
+    duration: 0.5,
+    translateY: -100,
+    opacity: 0,
+  });
+
+  gsap.to(menu, {
+    duration: 0.5,
+    translateY: 0,
+    opacity: 1,
+  });
+}
+
+function animateArticle(selector, translateXValue, delay = 0) {
+  gsap.from(selector, {
+    duration: 1.5,
+    translateX: translateXValue,
+    scaleY: 0.9,
+    delay: delay,
+    scrollTrigger: {
+      trigger: selector.parentElement,
+    },
+  });
+}
+
+animateArticle("#article-1", -600);
+animateArticle("#article-2", 600, 0.5);
+
+function animateTestimonials() {
+  gsap.from("#testimonials", {
+    opacity: 0,
+    duration: 2,
+    scale: 0,
+    scrollTrigger: {
+      trigger: "#testimonials-section",
+    },
+  });
+
+  gsap.to("#testimonials", {
+    opacity: 1,
+    duration: 2,
+    scale: 1,
+    scrollTrigger: {
+      trigger: "#testimonials-section",
+    },
+  });
+}
+
+animateTestimonials();
